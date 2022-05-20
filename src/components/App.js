f50dd06926e0b15ee275239358ce78d31f9d6eb7
Blain Crawford <blaincrawford@gmail.com>
2022-05-20 14:09:54 -0400

    have all pictures music and font uploaded

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+const App = () => {
+  const [cardArray, setCardArray] = useState([]);
+
+  return(
+    <div>
+      <h1>Hello, Adventure Time</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  resolve: {
+    fallback: {
+      process: require.resolve("process/browser"),
+      zlib: require.resolve("browserify-zlib"),
+      stream: require.resolve("stream-browserify"),
+      util: require.resolve("util"),
+      buffer: require.resolve("buffer"),
+      asset: require.resolve("assert"),
+    }
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, './build'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+      process: "process/browser",
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ]
+}
